9738589236b4af330548090de9d291914c22d88e
Shadrach Odekhiran <shadrach.odekhiran@gmail.com>
2019-10-27 19:38:40 +0100

    db migrate and model design

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
+use App\CustomersField;
+use App\Invoice;
+use App\InvoicesItem;
 use Illuminate\Http\Request;
 
 class InvoicesController extends Controller
@@ -11,6 +15,27 @@ class InvoicesController extends Controller
     }
 
     public function store(Request $request) {
-        return $request->all();
+        $customer = Customer::create($request->customer);
+        $invoice = Invoice::create($request->invoice + ['customer_id' => $customer->id]);
+        for ($i=0; $i < count($request->product); $i++) {
+            if ( isset($request->qty[$i]) && isset($request->price[$i]) ) {
+                InvoicesItem::create([
+                    'invoice_id' => $invoice->id,
+                    'name' => $request->product[$i],
+                    'quantity' => $request->qty[$i],
+                    'price' => $request->price[$i],
+                ]);
+            }
+        }
+
+        for ($i=0; $i < count($request->customer_fields); $i++) {
+            if ( isset($request->customer_fields[$i]['field_key']) && isset($request->customer_fields[$i]['field_value']) ) {
+                CustomersField::create([
+                    'customer_id' => $customer->id,
+                    'field_key' => $request->customer_fields[$i]['field_key'],
+                    'field_value' => $request->customer_fields[$i]['field_value'],
+                ]);
+            }
+        }
     }
 }
